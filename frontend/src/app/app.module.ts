ecc970df255f0e60347989e415998e433ca56450
Jan <jankopec7@gmail.com>
2023-08-29 20:37:47 +0200

    admin layout

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DefaultModule } from './layouts/default/default.module';
 import { FullpageModule } from './layouts/fullpage/fullpage.module';
-
+import { FullpageadminModule } from './layouts/fullpageadmin/fullpageadmin.module';
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +14,8 @@ import { FullpageModule } from './layouts/fullpage/fullpage.module';
     BrowserModule,
     AppRoutingModule,
     DefaultModule,
-    FullpageModule
+    FullpageModule,
+    FullpageadminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
