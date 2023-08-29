802ef3d6ac213e30ba32aad8f20ba9703e7f7a89
Jan <jankopec7@gmail.com>
2023-08-29 15:21:54 +0200

    reinitialized backend

--- backend/src/main/java/project/ztpai/shop/ShopApplication.java
+++ backend/src/main/java/project/ztpai/shop/ShopApplication.java
@@ -1,13 +1,13 @@
-package com.example.carrentalapp;
+package project.ztpai.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarRentalAppApplication {
+public class ShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CarRentalAppApplication.class, args);
+		SpringApplication.run(ShopApplication.class, args);
 	}
 
 }
--- backend/src/test/java/project/ztpai/shop/ShopApplicationTests.java
+++ backend/src/test/java/project/ztpai/shop/ShopApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.carrentalapp;
+package project.ztpai.shop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CarRentalAppApplicationTests {
+class ShopApplicationTests {
 
 	@Test
 	void contextLoads() {
