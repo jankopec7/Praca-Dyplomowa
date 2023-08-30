52da1a6c81ed11769d183d129c1f0a04e5222a29
Jan <jankopec7@gmail.com>
2023-08-30 19:23:10 +0200

    ERD diagram

--- frontend/src/app/shared/components/sidebar/sidebar.component.ts
+++ frontend/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,17 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent {
-
+export class SidebarComponent implements OnInit {
   categories  =['Category 1', 'Category 2','Category 3', 'Category 4', 'Category 5'];
   
-  onCategorySelected(category: any) {
-    // Zrób coś z wybraną kategorią
-    console.log("Selected category:", category);
-  }
+  constructor() { }
 
+  ngOnInit(): void {
+  }
 }
