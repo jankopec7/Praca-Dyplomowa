dc2825e09727a4b5e40ee529ee935966100b3f8c
Jan <jankopec7@gmail.com>
2024-01-18 04:05:33 +0100

    railway test4

--- frontend/server.js
+++ frontend/server.js
@@ -1,6 +1,25 @@
-const port = process.env.PORT || 3000;
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
 
+app.use(cors());
+
+const backendUrl = 'https://backend-production-c26d.up.railway.app/';  
+
+app.use(express.static(path.join(__dirname, 'dist/shop')));
+
+app.use('/api', (req, res) => {
+  apiProxy.web(req, res, { target: backendUrl });
+});
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/shop/index.html'));
+});
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
