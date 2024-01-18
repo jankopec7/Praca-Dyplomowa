553b190513f6bb61758582db361f17f328d6148b
Jan <jankopec7@gmail.com>
2024-01-18 03:53:59 +0100

    railway test3

--- frontend/server.js
+++ frontend/server.js
@@ -1,25 +1,3 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const path = require('path');
-const app = express();
-
-const backendUrl = process.env.BACKEND_URL || 'https://backend-production-c26d.up.railway.app/';
-
-const apiProxy = createProxyMiddleware('/api', {
-  target: backendUrl,
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': '',
-  },
-});
-
-app.use('/api', apiProxy);
-app.use(express.static(path.join(__dirname, 'dist/shop')));
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/shop/index.html'));
-});
-
 const port = process.env.PORT || 3000;
 
 
